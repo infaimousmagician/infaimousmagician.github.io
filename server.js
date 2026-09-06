ff991ce3184d77a6602b405157ba103d1500e616
infaimousmagician <brodudeguymanduderino@gmail.com>
2026-09-06 15:20:26 -0400

    feat: add favicon to application

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = 3000;
 
 app.use(express.static(__dirname));
 
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'clover.png'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
